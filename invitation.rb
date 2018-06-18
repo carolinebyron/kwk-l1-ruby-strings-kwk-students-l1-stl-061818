2453bff8edadd88f9ebccaed1abab0346d169eb8
carolinebyron <byroncaroline2@gmail.com>
2018-06-18 21:11:13 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,10 +1,5 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-Dear Hermione,
- 
-You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
- 
-Sincerely,
- 
-Harry Potter
+guest_name=Hermione
+party_name=
